75fd094a1fd025bf56e51d141d3b2559c216fd2e
Wangari-j <wangarijudy@gmail.com>
2024-02-11 15:53:50 +0300

    arrays

---
+++ loops.py
@@ -0,0 +1,10 @@
+colours = ["Red", "Green", "Pink", "Black", "Yellow", "Violet", "Blue", "Indigo"]
+colours.append("White")
+
+print(colours)
+colours.pop(3)
+print(colours)
+
+for m in colours:
+    print(m)
+    
